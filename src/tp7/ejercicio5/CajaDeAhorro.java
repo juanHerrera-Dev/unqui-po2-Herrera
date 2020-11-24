cba63ea809043ab9a8922184c468e2f8e66f76e2
juanHerrera-Dev <juanHerreraUnq@outlook.com.ar>
2020-11-23 21:00:25 -0300

    tp7-ejercicio5

---
+++ src/tp7/ejercicio5/CajaDeAhorro.java
@@ -0,0 +1,20 @@
+package tp7.ejercicio5;
+
+public class CajaDeAhorro extends CuentaBancaria {
+	
+    private int limite;
+    
+    public CajaDeAhorro(String titular, int limite){
+        super(titular);
+        this.limite=limite;
+    }
+    
+    public int getLimite(){
+        return this.limite;
+    }
+   
+    public boolean puedeExtraer(int monto) {
+        return this.getSaldo()>=monto && this.getLimite()>=monto;
+    }
+    
+}
